a0a16211dd33b89519b3fdba7843846551154dd5
Deepesh Dhakal <deepesh.dhakal@thamserku.com>
2021-09-21 15:37:39 +0545

    finished with cpt teams:

--- wp-content/themes/aashura/page-home.php
+++ wp-content/themes/aashura/page-home.php
@@ -296,81 +296,43 @@
       $args = ['post_type' => 'team', 'post_status' => 'publish', 'posts_per_page' => 4, 'order' => 'ASC'];
       $loop = new WP_Query($args);
       if($loop->have_posts()){
+        ?>
+          <div class="row">
+        <?php
         while ($loop->have_posts()){
           $loop->the_post();
-          echo "Yes";
+          $featured_img_url = get_the_post_thumbnail_url($post, 'full');
+          $altImage = get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true);
+          $job_position = get_post_meta($post->ID, 'job_position', true);
+          $twitter_url = get_post_meta($post->ID, 'twitter_profile_url', true);
+          $facebook_url = get_post_meta($post->ID, 'facebook_profile_url', true);
+          $instagram_url = get_post_meta($post->ID, 'instagram_profile_url', true);
+          $linkedin_url = get_post_meta($post->ID, 'linkedin_profile_url', true);
+          
+          ?>
+            <div class="col-lg-6 mt-4 mt-lg-0">
+          <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+            <div class="pic"><img src="<?php echo $featured_img_url?>" class="img-fluid" alt=""></div>
+            <div class="member-info">
+              <h4><?php echo the_title();?></h4>
+              <span><?php echo $job_position?></span>
+              <p><?php echo the_excerpt()?></p>
+              <div class="social">
+                <a href="<?php echo $twitter_url ?>"><i class="ri-twitter-fill"></i></a>
+                <a href="<?php echo $facebook_url ?>"><i class="ri-facebook-fill"></i></a>
+                <a href="<?php echo $instagram_url ?>"><i class="ri-instagram-fill"></i></a>
+                <a href="<?php echo $linkedin_url ?>"> <i class="ri-linkedin-box-fill"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+          <?php
         }
+        ?>
+        </div>
+        <?php
       } else echo "<h4>Please add some team members that will appear here";
       ?>
-        <div class="row">
-        <div class="col-lg-6 mt-4 mt-lg-0">
-          <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
-            <div class="pic"><img src="<?php echo get_template_directory_uri() ?>/assets/img/team/team-1.jpg" class="img-fluid" alt=""></div>
-            <div class="member-info">
-              <h4>Walter White</h4>
-              <span>Chief Executive Officer</span>
-              <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
-              <div class="social">
-                <a href=""><i class="ri-twitter-fill"></i></a>
-                <a href=""><i class="ri-facebook-fill"></i></a>
-                <a href=""><i class="ri-instagram-fill"></i></a>
-                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-6 mt-4 mt-lg-0">
-          <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
-            <div class="pic"><img src="<?php echo get_template_directory_uri() ?>/assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
-            <div class="member-info">
-              <h4>Sarah Jhonson</h4>
-              <span>Product Manager</span>
-              <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
-              <div class="social">
-                <a href=""><i class="ri-twitter-fill"></i></a>
-                <a href=""><i class="ri-facebook-fill"></i></a>
-                <a href=""><i class="ri-instagram-fill"></i></a>
-                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-6 mt-4">
-          <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="300">
-            <div class="pic"><img src="<?php echo get_template_directory_uri() ?>/assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
-            <div class="member-info">
-              <h4>William Anderson</h4>
-              <span>CTO</span>
-              <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
-              <div class="social">
-                <a href=""><i class="ri-twitter-fill"></i></a>
-                <a href=""><i class="ri-facebook-fill"></i></a>
-                <a href=""><i class="ri-instagram-fill"></i></a>
-                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-6 mt-4">
-          <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
-            <div class="pic"><img src="<?php echo get_template_directory_uri() ?>/assets/img/team/team-4.jpg" class="img-fluid" alt=""></div>
-            <div class="member-info">
-              <h4>Amanda Jepson</h4>
-              <span>Accountant</span>
-              <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
-              <div class="social">
-                <a href=""><i class="ri-twitter-fill"></i></a>
-                <a href=""><i class="ri-facebook-fill"></i></a>
-                <a href=""><i class="ri-instagram-fill"></i></a>
-                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
       </div>
 
     </div>
