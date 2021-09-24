f516608ace58f78bf9bb99c2d4111470fb61f6fa
Deepesh Dhakal <deepesh.dhakal@thamserku.com>
2021-09-24 15:29:05 +0545

    making multiple metaboxes work

--- wp-content/themes/aashura/inc/enqueue.admin.php
+++ wp-content/themes/aashura/inc/enqueue.admin.php
@@ -1,6 +1,8 @@
 <?php
 
 add_action('admin_enqueue_scripts', function($hook){
+		wp_register_style('aashura-admin-style', get_template_directory_uri() . '/assets/css/admin.style.css', array(), '1.0.0', 'all' );
+		wp_enqueue_style('aashura-admin-style');
 		wp_register_script('aashura-admin-script', get_template_directory_uri() . '/assets/js/adminScript.js', array('jquery'), '1.0.0', true);
 		wp_enqueue_script('aashura-admin-script');
 	}
--- wp-content/themes/aashura/inc/metaboxes/pricing.php
+++ wp-content/themes/aashura/inc/metaboxes/pricing.php
@@ -11,7 +11,7 @@ function aashura_add_metabox_pricing_cb($post)
 {
 	$pricing = get_post_meta($post->ID, 'pricing', true);
 ?>
-	<h4>Enter the pricing (US $). e.g: 10, 20,etc</h4>
+	<label for="pricing">Enter the pricing (US $). e.g: 10, 20,etc</label>
 	<input name="pricing" type="number" max="1000" min="0" value="<?php echo $pricing ?>">
 <?php
 }
@@ -20,10 +20,26 @@ function aashura_add_metabox_pricing_cb($post)
 function aashura_add_meta_box_feature_cb($post)
 {
 	$feature1 = get_post_meta($post->ID, 'feature1', true);
+	$feature2 = get_post_meta($post->ID, 'feature2', true);
+	$feature3 = get_post_meta($post->ID, 'feature3', true);
+	$feature4 = get_post_meta($post->ID, 'feature4', true);
+	$feature5 = get_post_meta($post->ID, 'feature5', true);
 ?>
 	<div id="features_container">
 		<div class="features_container__inner">
-			<input name="feature1" type="text" maxlength="80" value="<?php echo $feature1 ?>">
+			<input name="feature1" id="feature1" type="text" maxlength="80" value="<?php echo $feature1 ?>">
+		</div>
+		<div class="features_container__inner">
+			<input name="feature2" id="feature2" type="text" maxlength="80" value="<?php echo $feature2 ?>">
+		</div>
+		<div class="features_container__inner">
+			<input name="feature3" id="feature3" type="text" maxlength="80" value="<?php echo $feature3 ?>">
+		</div>
+		<div class="features_container__inner">
+			<input name="feature4" id="feature4" type="text" maxlength="80" value="<?php echo $feature4 ?>">
+		</div>
+		<div class="features_container__inner">
+			<input name="feature5" id="feature5" type="text" maxlength="80" value="<?php echo $feature5 ?>">
 		</div>
 	</div>
 <?php
@@ -53,13 +69,70 @@ function aashura_save_metabox_pricing($post_id, $post)
 		//delete
 		delete_post_meta($post_id, 'pricing');
 	}
+//save the features dynamically using array
+	$features = ['feature1', 'feature2', 'feature3', 'feature4', 'feature5'];
 	// update / delte features value
-	if (isset($_POST['feature1'])) {
-		update_post_meta($post_id, 'feature1', sanitize_text_field($_POST['feature1']), '');
-	} else {
-		//delete
-		delete_post_meta($post_id, 'feature1');
+	foreach ($features as $feature) {
+		if (isset($_POST[$feature])) {
+			update_post_meta($post_id, $feature, sanitize_text_field($_POST[$feature]), '');
+		} else {
+			//delete
+			delete_post_meta($post_id, $feature);
+		}
 	}
 }
 
+
 add_action('save_post', 'aashura_save_metabox_pricing', 10, 2);
+
+
+function wporg_add_custom_box() {
+    $screens = [ 'pricing' ];
+    foreach ( $screens as $screen ) {
+        add_meta_box(
+            'wporg_box_id',                 // Unique ID
+            'Custom Meta Box Title',      // Box title
+            'wporg_custom_box_html',  // Content callback, must be of type callable
+            $screen                            // Post type
+        );
+    }
+}
+add_action( 'add_meta_boxes', 'wporg_add_custom_box' );
+
+function wporg_custom_box_html( $post ) {
+    $value = get_post_meta( $post->ID, '_wporg_meta_key', true );
+    $value2 = get_post_meta( $post->ID, '_wporg_meta_key_2', true );
+    ?>
+    <label for="wporg_field">Description for this field</label>
+    <select name="wporg_field" id="wporg_field" class="postbox">
+        <option value="">Select something...</option>
+        <option value="something" <?php selected( $value, 'something' ); ?>>Something</option>
+        <option value="else" <?php selected( $value, 'else' ); ?>>Else</option>
+    </select>
+
+<label for="wporg_field2">Description for this field</label>
+    <select name="wporg_field2" id="wporg_field2" class="postbox">
+        <option value="">Select something...</option>
+        <option value="something" <?php selected( $value2, 'something' ); ?>>Something</option>
+        <option value="else" <?php selected( $value2, 'else' ); ?>>Else</option>
+    </select>
+    <?php
+}
+
+function wporg_save_postdata( $post_id ) {
+    if ( array_key_exists( 'wporg_field', $_POST ) ) {
+        update_post_meta(
+            $post_id,
+            '_wporg_meta_key',
+            $_POST['wporg_field']
+        );
+    }
+ if ( array_key_exists( 'wporg_field2', $_POST ) ) {
+        update_post_meta(
+            $post_id,
+            '_wporg_meta_key_2',
+            $_POST['wporg_field2']
+        );
+    }
+}
+add_action( 'save_post', 'wporg_save_postdata' );
